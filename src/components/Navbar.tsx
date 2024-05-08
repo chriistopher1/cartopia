328ae2e01c8ff80fac38800668528016be4b427c
Chris <christopherhuu77@gmail.com>
2024-05-08 22:00:19 +0700

    navbar revision

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,79 +1,72 @@
 import { Link } from "react-router-dom";
-import { FaSearch, FaHeart } from "react-icons/fa";
+import { FaSearch, FaHeart, FaUser } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import { IoMenu } from "react-icons/io5";
+
+const mobileViewCss = "w-full py-2 rounded-md text-center hover:text-white hover:bg-[#63a5ea]";
 
 const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+      <div className="w-screen flex flex-wrap items-center justify-around max-lg:justify-between p-4">
+        <Link to={"/"}>
+          <span className="self-center text-3xl font-bold whitespace-nowrap dark:text-white">
+            Cartopia
           </span>
-        </a>
+        </Link>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
           <span className="sr-only">Open main menu</span>
-          <FaCartShopping />
+          <IoMenu />
         </button>
-        <div className="w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
+        <div id="navbar-default" className="flex flex-col gap-2 py-5 items-center w-full lg:hidden ">
+          <Link to={""} className={`${mobileViewCss}`}>Home</Link>
+          <Link to={""} className={`${mobileViewCss}`}>Shop</Link>
+          <Link to={""} className={`${mobileViewCss}`}>About</Link>
+          <Link to={""} className={`${mobileViewCss}`}>Blog</Link>
+          <Link to={""} className={`${mobileViewCss}`}>Contact</Link>
+          <Link to={""} className={`${mobileViewCss}`}>Pages</Link>
+          <Link to={""} className={`flex justify-center items-center gap-2 ${mobileViewCss} `}>
+            <FaUser />
+            <span>Login/Register</span>
+          </Link>
+          <Link to={""} className={`flex justify-center ${mobileViewCss}`}>
+            <FaSearch />
+          </Link>
+          <Link to={""} className={`flex justify-center ${mobileViewCss}`}>
+            <FaCartShopping />
+          </Link>
+          <Link to={""} className={`flex justify-center ${mobileViewCss}`}>
+            <FaHeart />
+          </Link>
+        </div>
+        <div className="max-lg:hidden lg:flex gap-5">
+          <Link to={""}>Home</Link>
+          <Link to={""}>Shop</Link>
+          <Link to={""}>About</Link>
+          <Link to={""}>Blog</Link>
+          <Link to={""}>Contact</Link>
+          <Link to={""}>Pages</Link>
+        </div>
+        <div className="max-lg:hidden lg:flex items-center gap-5">
+          <Link to={""} className="flex items-center gap-2 text-[#63a5ea]">
+            <FaUser />
+            <span>Login/Register</span>
+          </Link>
+          <Link to={""} className="text-[#63a5ea]">
+            <FaSearch />
+          </Link>
+          <Link to={""} className="text-[#63a5ea]">
+            <FaCartShopping />
+          </Link>
+          <Link to={""} className="text-[#63a5ea]">
+            <FaHeart />
+          </Link>
         </div>
       </div>
     </nav>
