6dd68b4fc410ce9deca07797084c4e870002434c
Chris <christopherhuu77@gmail.com>
2024-05-16 20:45:47 +0700

    recoomend

--- src/components/dashboard/Recommend.tsx
+++ src/components/dashboard/Recommend.tsx
@@ -10,12 +10,14 @@ const Recommend = () => {
 
   return (
     <div className="mx-4 md:mx-24 mb-5">
-      <h2 className="border-b-2 border-black mb-5 text-lg md:text-2xl font-bold pb-4">Popular Product</h2>
-      <div className="flex gap-5 flex-wrap  ">
+      <h2 className="border-b-2 border-black mb-5 text-lg md:text-2xl font-bold pb-4">
+        Popular Product
+      </h2>
+      <div className="flex flex-wrap gap-6 w-full">
         {allProduct &&
           allProduct.map((product) => (
             <ProductCard
-              key={product?.name}
+              key={product?.id}
               category={product?.category}
               description={product?.description}
               imageUrl={product?.imageUrl}
