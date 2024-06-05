ef8d729cb5592a89b517d925f712dc0809bdb018
Revlyzl <reagenpinontoan510@gmail.com>
2024-06-05 12:48:02 +0700

    test

--- src/components/dashboard/Category.tsx
+++ src/components/dashboard/Category.tsx
@@ -6,7 +6,7 @@ const Category = () => {
   const { data: categories, isPending } = useGetCategoryAsset()
 
   if(isPending) return <div>Loading..</div>
-
+  console.log(categories)
   return (
     <div className="mx-4 md:mx-24 py-12">
       <h2 className="border-b-2 border-black mb-5 text-lg md:text-2xl font-bold pb-4">Category</h2>
